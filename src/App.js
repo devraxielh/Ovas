8b952fd141d990b8d44809c74176a813b4fd3832
Rodrigo Garcia <dev.raxielh@gmail.com>
2024-10-14 18:43:20 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-//import regresion_lineal_interactiva from './components/regresion_lineal_interactiva';
 import RegresionLineal from './components/RegresionLineal';
 const App = () => {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/RegresionLineal" element={<RegresionLineal />} />
+          <Route path="/regresionlineal" element={<RegresionLineal />} />
         </Routes>
       </div>
     </Router>
